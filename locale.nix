84e28eacecd1b2907d8a4ed370f1f5247975a72c
bird <dark_soul_bird@proton.me>
2026-03-21 00:33:37 -0300

    feat(base): add locale config

---
+++ locale.nix
@@ -0,0 +1,21 @@
+# locale.nix
+{ config, pkgs, ... }:
+
+{
+  console.keyMap = "la-latin1";
+
+  time.timeZone = "America/Argentina/Buenos_Aires";
+
+  i18n.defaultLocale = "es_AR.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_AR.UTF-8";
+    LC_IDENTIFICATION = "es_AR.UTF-8";
+    LC_MEASUREMENT = "es_AR.UTF-8";
+    LC_MONETARY = "es_AR.UTF-8";
+    LC_NAME = "es_AR.UTF-8";
+    LC_NUMERIC = "es_AR.UTF-8";
+    LC_PAPER = "es_AR.UTF-8";
+    LC_TELEPHONE = "es_AR.UTF-8";
+    LC_TIME = "es_AR.UTF-8";
+  };
+}
